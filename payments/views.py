08470e2608facea4018ceb09e23a8ed27f1f6449
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-06-01 17:28:05 +0900

    220601_create approval views&html

--- payments/views.py
+++ payments/views.py
@@ -17,7 +17,7 @@ def home(request):
             "quantity": "1",                # 구매 물품 수량
             "total_amount": "12000",        # 구매 물품 가격
             "tax_free_amount": "0",         # 구매 물품 비과세
-            "approval_url": "http://127.0.0.1:8000/admin",
+            "approval_url": "http://127.0.0.1:8000/approval",
             "cancel_url": "http://127.0.0.1:8000/",
             "fail_url": "http://127.0.0.1:8000/",
         }
@@ -28,3 +28,25 @@ def home(request):
         return redirect(next_url)
 
     return render(request, 'home.html')
+
+def approval(request):
+    URL = 'https://kapi.kakao.com/v1/payment/approve'
+    headers = {
+            "Authorization": "KakaoAK " + "88ca1875a9b3176b59b0098a0345ed1e",
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        }
+    params = {
+        "cid": "TC0ONETIME",    # 테스트용 코드
+        "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
+        "partner_order_id": "1001",     # 주문번호
+        "partner_user_id": "german",    # 유저 아이디
+        "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
+    }
+    response = requests.post(URL, headers=headers, params=params)
+    amount = response.json()['amount']['total']
+    response = response.json()
+    context = {
+        'response': response,
+        'amount': amount,
+    }
+    return render(request, 'approval.html', context)
