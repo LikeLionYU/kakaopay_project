cd6778c0104130fa9bafa75d48e5b97164048579
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-06-01 21:55:24 +0900

    220601_connect kakaopay api with models&forms

--- payments/models.py
+++ payments/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Shopping(models.Model):
     item_name = models.CharField(max_length=100)
+    customer_name = models.CharField(max_length=20)
     item_price = models.IntegerField()
     quantity = models.IntegerField()
     total_amount = models.IntegerField()
-    tax_free_amount = models.FloatField()
+    tax_free_amount = models.IntegerField()
     shopped_date = models.DateTimeField(auto_now_add=True)
-
+    is_complete = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.item_name
-
--- payments/views.py
+++ payments/views.py
@@ -1,55 +1,72 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 import requests
+from .models import Shopping
 from .forms import ShoppingForm
 
 def home(request):
     if request.method == 'POST':
-        URL = 'https://kapi.kakao.com/v1/payment/ready'
-        headers = {
-            "Authorization": "KakaoAK " + "88ca1875a9b3176b59b0098a0345ed1e",
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        }
+        first_form = ShoppingForm(request.POST)
+        if first_form.is_valid():
+            # 결제 단계 넘어가면 일단, Shopping Table에 데이터 추가(대신, 구매 확정은 False)
+            form = first_form.save(commit=False)
+            form.total_amount = int(form.quantity)*int(form.item_price)
+            form.save()
 
-        params = {
-            "cid": "TC0ONETIME",    # 테스트용 제휴코드
-            "partner_order_id": "1001",     # 주문번호
-            "partner_user_id": "german",    # 유저 아이디
-            "item_name": "연어초밥",        # 구매 물품 이름
-            "quantity": "1",                # 구매 물품 수량
-            "total_amount": "12000",        # 구매 물품 가격
-            "tax_free_amount": "0",         # 구매 물품 비과세
-            "approval_url": "http://127.0.0.1:8000/approval",
-            "cancel_url": "http://127.0.0.1:8000/",
-            "fail_url": "http://127.0.0.1:8000/",
-        }
+            URL = 'https://kapi.kakao.com/v1/payment/ready'
+            headers = {
+                "Authorization": "KakaoAK " + "88ca1875a9b3176b59b0098a0345ed1e",
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            }
+            params = {
+                "cid": "TC0ONETIME",    # 테스트용 제휴코드
+                "partner_order_id": form.id,     # 주문번호
+                "partner_user_id": form.customer_name,    # 유저 아이디
+                "item_name": form.item_name,        # 구매 물품 이름
+                "quantity": form.quantity,                # 구매 물품 수량
+                "total_amount": form.total_amount,        # 구매 물품 가격
+                "tax_free_amount": form.tax_free_amount,         # 구매 물품 비과세
+                "approval_url": "http://127.0.0.1:8000/approval",
+                "cancel_url": "http://127.0.0.1:8000/",
+                "fail_url": "http://127.0.0.1:8000/",
+            }
+            response = requests.post(URL, headers=headers, params=params)
 
-        response = requests.post(URL, headers=headers, params=params)
-        request.session['tid'] = response.json()['tid']      # 결제 승인시 사용할 tid를 세션에 저장
-        next_url = response.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
-        return redirect(next_url)
+            request.session['tid'] = response.json()['tid']      # 결제 승인시 사용할 tid를 세션에 저장
+            request.session['order_id'] = form.id      # 결제 승인시 사용할 order_id를 세션에 저장
+            request.session['customer_name'] = form.customer_name      # 결제 승인시 사용할 customer_name을 세션에 저장
+            next_url = response.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
+            return redirect(next_url)
+
     if request.method == "GET":
-        form = ShoppingForm
-        context = {'form':form }
+        form = ShoppingForm()
+        context = {'form':form}
         return render(request, 'home.html', context)
 
+
 def approval(request):
-    URL = 'https://kapi.kakao.com/v1/payment/approve'
-    headers = {
-            "Authorization": "KakaoAK " + "88ca1875a9b3176b59b0098a0345ed1e",
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    if request.method == 'GET':
+        # 결제 승인단계에서 해당 쇼핑을 구매 확정 처리
+        order_id = request.session['order_id']
+        shopped_history = get_object_or_404(Shopping, pk=order_id)
+        shopped_history.is_complete = True
+        shopped_history.save()
+
+        URL = 'https://kapi.kakao.com/v1/payment/approve'
+        headers = {
+                "Authorization": "KakaoAK " + "88ca1875a9b3176b59b0098a0345ed1e",
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            }
+        params = {
+            "cid": "TC0ONETIME",    # 테스트용 코드
+            "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
+            "partner_order_id": order_id,     # 주문번호
+            "partner_user_id": request.session['customer_name'],    # 유저 아이디
+            "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
         }
-    params = {
-        "cid": "TC0ONETIME",    # 테스트용 코드
-        "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-        "partner_order_id": "1001",     # 주문번호
-        "partner_user_id": "german",    # 유저 아이디
-        "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
-    }
-    response = requests.post(URL, headers=headers, params=params)
-    amount = response.json()['amount']['total']
-    response = response.json()
-    context = {
-        'response': response,
-        'amount': amount,
-    }
-    return render(request, 'approval.html', context)
+        response = requests.post(URL, headers=headers, params=params)
+        amount = response.json()['amount']['total']
+        context = {
+            'response': response.json(),
+            'amount': amount,
+        }
+        return render(request, 'approval.html', context)
